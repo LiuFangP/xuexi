531f33bc0ecd68a504dbac1fad1118086b0af8de
LiuFangP <liufingjiu@163.com>
2020-03-26 10:22:46 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,66 @@
+# print("hello world!")  # 字符串,print--打印，输出
+# print(23333)  # 整数
+# print(True)  #  布尔值
+# print(())   #  元组
+# print([])   #  数组
+# print({})   #  字典
+# print("哈哈",233333,2.333)
+# print("哈哈"+"嘻嘻")  # 字符串的拼接
+# print("哈哈"*10)  #  10个哈哈，即20个哈
+# print(((1+2)*100-99)/2)
+# print(2>3)
+# print(1+1==2)
+# # 获取，输入  变量：给某个值取的名字；赋值：把某个值交给变量
+# a = "张三"   # 把张三这个值赋值给了名字叫a的这个变量
+# print(a)   
+# a = input("请输入:")
+# b = input("请输入:")
+# print("input获取的内容：",a+b)
+
+# 数据格式的转换：整数用float转换会转换成小数，eg：10——>10.0
+
+# print(type("哈哈"))
+# print(type(23333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+"""
+练习：通过代码获取两段内容，并且计算他们的长度的和。
+
+"""
+
+
+
+# print(2>3)
+# print(1+9==10)
+
+# a = input("请输入：")
+# b = input("请输入：")
+# print("input获取的内容：",a+b)
+
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# c = float(input("请输入："))
+# print("input获取的内容：",a+b*c)
+
+# a = input("请输入：")
+# b = input("请输入：")
+# c = input("请输入：")
+# print("input获取的内容：",len(a)+len(c))
+
+# a = int(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+
+# a = "张三是个王麻子的赚十万 劳 务"
+# print(len(a))
+
+
+
+a = input("请输入：")
+b = input("请输入：")
+print("两段字符串的长度是：",len(a)+len(b))
